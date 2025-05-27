999d936cac27b2794847c73787454893df4c6ee1
Madan Golay Tamang <madantamang939@gmail.com>
2025-05-27 22:20:54 +0545

    new

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Roboto, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import FloatingIcons from "@/components/FloatingIcons";
+
+const roboto = Roboto({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "SRIYOG Consulting | IT Consulting Company in Nepal",
+  description: "",
+  icons: "/assets/images/title/logo.svg",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${robotoMono.variable} antialiased`}>
+        <Header />
+        <Navbar />
+        {children}
+        <FloatingIcons />
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+export default function Header() {
+  const [topOpen, setTopOpen] = useState(true);
+
+  return (
+    <>
+      <header className="w-full h-auto">
+        {topOpen && (
+          <section className="bg-[#0D5D59] w-full py-4">
+            <div className="lg:max-w-[1180px] max-lg:container max-lg:px-3 mx-auto text-white flex justify-between items-center lg:text-[14px] sm:text-[12px] text-[14px]">
+              <div className="flex gap-2">
+                <p>Helpline Number</p>
+                <Link href="tel:+977-98511-52774">+977-98511-52774</Link>
+              </div>
+              <div className="gap-2 flex items-center">
+                <i className="hidden md:block">
+                  SRIYOG App is having its own homepage and a new company.
+                </i>
+                <Link
+                  href=""
+                  className="py-[3px] px-1 mx-2 hidden md:block rounded-lg border-2 italic font-semibold"
+                  target="_blank"
+                >
+                  View Now
+                </Link>
+                <button
+                  onClick={() => setTopOpen(false)}
+                  className="border-2 w-[30px] h-[30px] rounded-lg font-semibold hover:cursor-pointer"
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          </section>
+        )}
+       
+      </header>
+    </>
+  );
+}
